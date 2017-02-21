be84ab474fdb8495c1cc8432a9a900ae1f5abf54
qyzxg <qyzxg@live.com>
2017-02-21 22:13:37 +0800

    优化

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
-from flask_pagedown import PageDown
 from flask_mail import Mail
 import flask_whooshalchemyplus
-
+from flaskext.markdown import Markdown
 
 app = Flask(__name__, static_folder='static')
 app.config.from_pyfile('configs.py')
@@ -15,8 +14,7 @@ login_manager.init_app(app)
 login_manager.login_message = '要访问该页面请您登录!'
 login_manager.needs_refresh_message = '请验证后再登录!'
 login_manager.login_view = 'login'
-pagedown = PageDown()
-pagedown.init_app(app)
+Markdown(app)
 Bootstrap(app)
 mail = Mail(app)
 flask_whooshalchemyplus.init_app(app)
